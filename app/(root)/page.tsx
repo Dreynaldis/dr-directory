f1433e07bbe005faa2b04b63100073d2024020d0
Dreynaldis <dion.reynaldis@gmail.com>
2024-11-07 11:05:03 +0700

    fixed typo

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,10 +2,10 @@ import SearchForm from "@/components/SearchForm";
 import StartupCard from "@/components/StartupCard";
 
 export default async function Home({ searchParams }: {
-  searchParams : Promise<{query?: string}>
+  searchParams: Promise<{ query?: string }>
 }) {
   const query = (await searchParams).query
-  const params = {search: query || null}
+  const params = { search: query || null }
 
   const posts = [{
     _createdAt: new Date(),
@@ -15,15 +15,14 @@ export default async function Home({ searchParams }: {
     description: "This is a description",
     image: "https://images.unsplash.com/photo-1634912314704-c646c586b131?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.03&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     category: "Robots",
-    title : "We Robots"
+    title: "We Robots"
   }]
-
-
+  
   return (
     <>
       <section className="pink_container">
         <h1 className="heading">
-          Pitch Your Startup, <br /> Connect With Entrepreuners  
+          Pitch Your Startup, <br /> Connect With Entrepreuners
         </h1>
         <p className="sub-heading !max-w-3xl">
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
@@ -47,5 +46,5 @@ export default async function Home({ searchParams }: {
         </ul>
       </section>
     </>
-  );
+  )
 }
